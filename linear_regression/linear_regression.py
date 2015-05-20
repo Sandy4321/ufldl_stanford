a7443e62678bf48499a6a4dc159f282ee341b7d1
madan-ram <madan_ram@rocketmail.com>
2015-05-20 13:40:30 +0530

    added linear regression

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -1,34 +1,55 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+# def get_data():
+# 	X = []
+# 	y = []
+# 	fr = open('ex3Data/ex3x.dat')
+# 	for x in fr:
+# 		X.append(map(float, x.strip().split()))
+# 	fr = open('ex3Data/ex3y.dat')
+# 	for x in fr:
+# 		y.append(map(float, x.strip().split()))
+# 	return (np.asarray(X), np.asarray(y))
+
 def get_data():
 	X = []
 	y = []
-	fr = open('ex3Data/ex3x.dat')
+	fr = open('ex3Data/ex1data1.txt')
 	for x in fr:
-		X.append(map(float, x.strip().split()))
-	fr = open('ex3Data/ex3y.dat')
-	for x in fr:
-		y.append(map(float, x.strip().split()))
-	return (np.asarray(X), np.asarray(y))
+		data = map(float, x.strip().split(','))
+		X.append(data[0])
+		y.append(data[1])
+	X = np.asarray(X)
+	X = X.reshape((X.shape[0], 1))
+	y = np.asarray(y)
+	y = y.reshape((y.shape[0], 1))
+	return (X, y)
 
 #define linear hypothysis return y
 def linear_h(X, theta):
-	temp = X.dot(theta)
-	return temp.reshape((temp.shape[0], 1)) 
+	return X.dot(theta)
 
 #mean square error function
 def error_func(y_dash, y):
 	return np.sum(0.5*(y_dash - y)**2, axis=1)
 
+def gradient(theta, X, y):
+	return X.T.dot(linear_h(X, theta) - y)
 
 X, y = get_data()
 
-#change X to inculde bias
+#change X to inculde bias (Add a column of ones to x)
 X_one = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
 
+
+epoch = 1500
+alpha = 0.01
+m, n = X_one.shape
+
 #define theta intialize to random
-theta = np.random.random((X_one.shape[1]))
+# theta = np.random.random((n, 1))
+theta = np.zeros((n, 1))
 
 #our intial prediction
 y_dash = linear_h(X_one, theta) 
@@ -39,8 +60,35 @@ error = error_func(y_dash, y)
 #overall/total average cost
 cost = np.mean(error, axis=0)
 
-print cost
+print cost, '<- intial cost'
 
-#cost generated
-# plt.plot([x[0] for x  in X], [x[0] for x in y], 'r*')
-# plt.show()
+#update theta
+
+for _ in xrange(epoch):
+	
+	delta = - alpha/m * gradient(theta, X_one, y)
+	theta += delta 
+
+	#intial error
+	error = error_func(linear_h(X_one, theta), y)
+
+	#overall/total average cost
+	cost = np.mean(error, axis=0)
+
+	# print cost
+
+predict1 = np.asarray([1, 3.5]).dot(theta)
+predict2 = np.asarray([1, 7]).dot(theta)
+
+print predict1, predict2
+
+size = 25
+plt.figure(figsize=(10, 8))
+plt.plot(y, X, 'r*')
+X_temp = np.asarray(xrange(0, size)).reshape((size, 1))
+y_temp = np.concatenate((np.ones((size, 1)), X_temp), axis=1).dot(theta)
+plt.plot(y_temp, X_temp, label='predicted')
+plt.legend(loc='upper left')
+plt.ylabel('House price ($1000s)')
+plt.xlabel('House #')
+plt.show()
---
+++ linear_regression/temp_test/linear_regression_housing_data_sgd_no_scaleing.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_data():
+	data_original = np.loadtxt('housing.data')
+	data = np.insert(data_original, 0, 1, axis=1)
+	np.random.shuffle(data)
+
+	train_X = data[:400, :-1]
+	train_y = data[:400, -1]
+
+	test_X = data[400:, :-1]
+	test_y = data[400:, -1]
+	return (train_X, train_y.reshape(train_y.shape[0], 1))
+
+#define linear hypothysis return y
+def linear_h(X, theta):
+	return X.dot(theta)
+
+#mean square error function
+def error_func(y_dash, y):
+	return np.sum(0.5*(y_dash - y)**2, axis=1)
+
+def gradient(theta, X, y):
+	return X.T.dot(linear_h(X, theta) - y)
+
+X, y = get_data()
+
+alpha = 0.000006
+m, n = X.shape
+
+
+#define theta intialize to random
+theta = np.random.random((n, 1))
+
+#our intial prediction
+y_dash = linear_h(X, theta) 
+
+#intial error
+error = error_func(y_dash, y)
+
+#overall/total average cost
+cost = np.mean(error, axis=0)
+
+print cost
+
+prev_cost = None
+#update theta
+while(True):
+
+	delta = - alpha/m * gradient(theta, X, y)
+	theta += delta 
+
+	#intial error
+	error = error_func(linear_h(X, theta), y)
+
+	#overall/total average cost
+	cost = np.mean(error, axis=0)
+
+	print cost
+	if prev_cost != None and prev_cost - cost <= 0.000005:
+		break
+	prev_cost = cost
+
+print theta
+
+
+plt.figure(figsize=(10, 8))
+plt.scatter(np.arange(y.size), sorted(y), c='b', edgecolor='None', alpha=0.5, label='actual')
+plt.scatter(np.arange(y.size), sorted(X.dot(theta)), c='g', edgecolor='None', alpha=0.5, label='predicted')
+plt.legend(loc='upper left')
+plt.ylabel('House price ($1000s)')
+plt.xlabel('House #')
+plt.show()
